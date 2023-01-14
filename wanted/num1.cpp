e1e8102e32da9921bb162b6887ca1b1e01a576c5
kxxhyorim <rlagyfla0709@gmail.com>
2023-01-14 14:38:55 +0900

    [num1] 18%

--- wanted/num1.cpp
+++ wanted/num1.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+int n, leftN = 0, rightN = 0, max_enlightenment = 0;
+bool visited[100001];
+vector<int> direction;
+
+
+int bfs(int x) { //index
+    visited[x] = true;
+    int count = 0;
+
+    queue<int> q;
+    q.emplace(x);
+
+    while (!q.empty()) {
+        count++;
+        x = q.front();
+        q.pop();
+        int nx = x + 1;
+        if (nx <= n && !visited[nx]) {
+            if (direction[x] == direction[nx]) {
+                q.emplace(nx);
+                visited[nx] = true;
+            }
+        }
+    }
+
+    if (count == 1)
+        return 0;
+
+    return count;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, left = 0, right = 0, max_enlightenment = 0;
     cin >> n;
-    vector<int> direction(n);
+    direction.resize(n);
 
-
-    cin >> direction[0];
-
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> direction[i];
-        if (direction[i] == direction[i - 1]) {
-            if (direction[i] == 1) {
-                left++;
-                if (i == 1) left++;
-            } else {
-                right++;
-                if (i == 1) right++;
-            }
+    }
+
+    // bfs
+    for (int i = 0; i < n; i++) {
+
+
+        if (direction[i] == 1 && !visited[i]) {
+            leftN += bfs(i);
+        } else if (direction[i] == 2 && !visited[i]) {
+            rightN += bfs(i);
         }
     }
 
@@ -38,8 +68,8 @@ int main() {
         return 0;
     }
 
-    max_enlightenment = max(left, right);
-    cout << max_enlightenment << endl;
+    cout << max(leftN, rightN);
+
 
     return 0;
 }
